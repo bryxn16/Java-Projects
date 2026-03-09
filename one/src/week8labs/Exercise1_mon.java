6d644d1c3b1c10f465e729d3c8e3313cd6b5f931
bguzman36 <bguzman36@WH-F144-09-LB.campus.csudh.edu>
2026-03-09 12:38:35 -0700

    update

---
+++ one/src/week8labs/Exercise1_mon.java
@@ -0,0 +1,30 @@
+package week8labs;
+
+public class Exercise1_mon {
+
+	public static void main(String[] args) 
+	{
+		int [] employeeNumbers = new int[100];
+		
+		double [] payRates = new double [25];
+		
+		char [] letters = new char [1000];
+		
+		String [] names = new String [10];
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

d62cce4619e53773efee7876b483dced189f08df
bguzman36 <bguzman36@WH-F144-09-LB.campus.csudh.edu>
2026-04-27 11:23:42 -0700

    4/27/26

---
+++ one/src/week14labs/CellPhone.java
@@ -0,0 +1,61 @@
+package week14labs;
+
+public class CellPhone 
+{
+	private String manufact;
+	private String model;
+	private double retailPrice;
+	
+	//overloaded constructor (HAS ARUGUMENTS)
+	public CellPhone(String newMan, String newMod, double newPrice)
+	{
+		manufact = newMan;
+		model = newMod;
+		retailPrice = newPrice;
+	}
+	
+	//start of ACCESSORS: setters
+	public void setManufact(String man)
+	{
+		manufact = man;
+	}//end of setManufact
+	public void setModel(String mod)
+	{
+		model = mod;
+	}
+	public void setretailPrice(double price)
+	{
+		retailPrice = price;
+	}
+	
+	//start of MUTATORS: getters
+	public String getManufact()
+	{
+		return manufact;
+	}//end of getManufact
+	public String getModel()
+	{
+		return model;
+	}//end of getModel
+	public double getretailPrice()
+	{
+		return retailPrice;
+	}//end of getretailPrice
+	
+	public void displayCellPhone(CellPhone c)
+	{
+		System.out.println("Manufacturer: " + manufact);
+		System.out.println("Model: " + model);
+		System.out.println("Retail Price $" + retailPrice);
+	}
+	
+	
+	
+	
+	
+	
+}//end of class
+
+
+
+
---
+++ one/src/week14labs/CellPhoneDemo.java
@@ -0,0 +1,39 @@
+package week14labs;
+import java.util.Scanner;
+public class CellPhoneDemo {
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the manufacturer: ");
+		String manufacturer = input.nextLine();
+		
+		System.out.println("Enter the model: ");
+		String model = input.nextLine();
+		
+		System.out.println("Enter the retail price: ");
+		double price = input.nextInt();
+		
+		CellPhone cp = new CellPhone(manufacturer, model, price); 
+		
+		System.out.println("\nHere is the data that you provided:");
+		System.out.println("Manufacturer: " + cp.getManufact());
+		System.out.println("Model: " + cp.getModel());
+		System.out.println("Retail Price $" + cp.getretailPrice());
+
+		System.out.println("What is the new price of your phone:");
+		price = input.nextInt();
+		
+		cp.getretailPrice();
+		
+		System.out.println("\nHere is the NEW data that you provided:");
+		cp.displayCellPhone(cp);
+		
+		
+		
+		
+		input.close();
+	}
+
+}

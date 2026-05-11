b87d39d907504a2907a90c35a8c33a0e7e5d4ab7
mgr72 <mgr72@192.168.2.15>
2026-05-10 17:26:26 -0700

    1

--- one/src/help/ArrayPractice.java
+++ one/src/help/ArrayPractice.java
@@ -1,14 +1,12 @@
 package help;
 
-import java.lang.reflect.Array;
-
 public class ArrayPractice {
 
 	public static void main(String[] args) 
 	{
 		int [] num = {1,2,3,5,6,7,8,9,10};
 		
-		int length = Array.getLength(num);
+		int length = num.length;
 		int sumNoMiss = 0;
 		int sumMiss = 0;
 		for(int i=0; i<num.length; i++)  //this adds up each array value 
